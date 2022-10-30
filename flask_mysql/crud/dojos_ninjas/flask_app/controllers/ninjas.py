36dc3b406c13808d513cf35cb6a24f8d0d582df3
Sophie Slagle <slagsoph@isu.edu>
2022-10-30 16:50:51 -0700

    Update and delete now both work for ninjas

--- flask_mysql/crud/dojos_ninjas/flask_app/controllers/ninjas.py
+++ flask_mysql/crud/dojos_ninjas/flask_app/controllers/ninjas.py
@@ -1,15 +1,37 @@
 from flask import render_template, redirect, request
 
 from flask_app import app
-from flask_app.models import ninja, dojo
+from flask_app.models.dojo import Dojo
+from flask_app.models.ninja import Ninja
 
 
 
 @app.route('/ninjas')
 def ninjas():
-    return render_template("ninjas.html", dojos=dojo.Dojo.get_all())
+    return render_template("ninjas.html", dojos=Dojo.get_all())
 
 @app.route('/process', methods=['POST'])
 def new_ninja():
-    ninja.Ninja.save(request.form)
+    Ninja.save(request.form)
     return redirect('/')
+
+@app.route('/ninja/destroy/<int:id>')
+def destroy(id):
+    data = {
+        'id':id
+    }
+    Ninja.destroy(data)
+    return redirect('/')
+
+@app.route('/ninja/edit', methods=['POST'])
+def update():
+    Ninja.update(request.form)
+    return redirect('/')
+
+@app.route('/ninja/edit/<int:id>')
+def edit(id):
+    data={
+        "id":id #variable id is the actual id number
+    }
+    ninja=Ninja.get_one(data)
+    return render_template('edit_ninja.html', ninja = ninja)
--- flask_mysql/crud/dojos_ninjas/flask_app/models/ninja.py
+++ flask_mysql/crud/dojos_ninjas/flask_app/models/ninja.py
@@ -25,19 +25,19 @@ class Ninja:
         result = connectToMySQL('dojos_ninjas').query_db(query,data)
         return result
     
-    # @classmethod
-    # def get_one(cls, data):
-    #     query="SELECT * FROM ninjas WHERE id = %(id)s;"
-    #     result= connectToMySQL('dojos_ninjas').query_db(query, data)
-    #     return cls(result[0]) #will only return the one id rather than all of the information 
+    @classmethod
+    def get_one(cls, data):
+        query="SELECT * FROM ninjas WHERE id = %(id)s;"
+        result= connectToMySQL('dojos_ninjas').query_db(query, data)
+        return cls(result[0]) #will only return the one id rather than all of the information 
     
-    # @classmethod
-    # def update(cls, data):
-    #     query="UPDATE ninjas SET first_name=%(first_name)s, last_name=%(last_name)s, age=%(age)s, WHERE id=%(id)s;"
-    #     return connectToMySQL('dojos_ninjas').query_db(query, data)
+    @classmethod
+    def update(cls, data):
+        query="UPDATE ninjas SET first_name=%(first_name)s, last_name=%(last_name)s, age=%(age)s WHERE id=%(id)s;"
+        return connectToMySQL('dojos_ninjas').query_db(query, data)
     
-    # @classmethod
-    # def destroy(cls, data):
-    #     query = "DELETE FROM ninjas WHERE id = %(id)s;"
-    #     return connectToMySQL('dojos_ninjas').query_db(query,data)
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM ninjas WHERE id = %(id)s;"
+        return connectToMySQL('dojos_ninjas').query_db(query,data)
     
